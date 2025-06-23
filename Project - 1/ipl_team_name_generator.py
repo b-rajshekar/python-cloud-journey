52631d317d16d66a54b72d88d7e6dd873854a138
Bujjagowns Rajashekhar <brajashekhar19@gmail.com>
2025-06-23 06:42:52 +0530

    Initial commit

---
+++ Project - 1/ipl_team_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the IPL Team Name Generator.")
+city = input("Type the name of an Indian city:\n")
+animal = input("Type your favorite animal:\n")
+
+print("Your IPL team name could be: " + city + " " + animal)
